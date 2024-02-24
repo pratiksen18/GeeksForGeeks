854abc5bea33f5064c7672af2743be19cc6d9ab5
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2024-02-24 21:07:38 +0530

    sort-the-array.java

--- Basic/Sort The Array/sort-the-array.java
+++ Basic/Sort The Array/sort-the-array.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-import java.io.*;
-import java.util.*; 
-
-
-class GFG{
-    public static void main(String args[]) throws IOException { 
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t-- > 0){
-            int n = sc.nextInt(); 
-            int arr[] = new int[n];
-            for (int i = 0; i < n; ++i)
-            {
-                arr[i] = sc.nextInt();
-            }
-            Solution obj = new Solution();
-            int []ans= obj.sortArr(arr, n);
-            for(int i=0;i<n;i++)
-            {
-                System.out.print(ans[i]+" ");
-            }
-            System.out.println();
-        }
-        
-    }
-}
-// } Driver Code Ends
-
 // Time Complexity: O(n * log n)
 // Auxiliary Space: O(1)
 
@@ -38,4 +7,4 @@ class Solution {
         Arrays.sort(arr);
         return arr;
     }
-} 
+}
