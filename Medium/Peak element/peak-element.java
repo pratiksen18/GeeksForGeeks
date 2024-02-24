fe3498150dc8e2920b15de889c0f32ef3b9a9bd3
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2024-02-24 20:51:44 +0530

    peak-element.java
    Code only

--- Medium/Peak element/peak-element.java
+++ Medium/Peak element/peak-element.java
@@ -1,81 +1,36 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.io.*;
-class PeakElement{
-	public static void main(String[] args)throws IOException{
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out=new PrintWriter(System.out);
-		int t=Integer.parseInt(in.readLine().trim());
-		while(t-->0){
-			int n=Integer.parseInt(in.readLine().trim());
-			int[] a=new int[n];
-		    int[] tmp=new int[n];
-		    String s[]=in.readLine().trim().split(" ");
-			for(int i=0;i<n;i++){
-				a[i]=Integer.parseInt(s[i]);
-				tmp[i] = a[i];
-			}
-		    
-		    int f=0;
-			int A=(new Solution()).peakElement(tmp,n);
-			
-			if(A<0 && A>=n)
-			    out.println(0);
-			else
-			{
-    			if(n==1 && A==0)
-    			    f=1;
-    			else if(A==0 && a[0]>=a[1])
-    			    f=1;
-    			else if(A==n-1 && a[n-1]>=a[n-2])
-    			    f=1;
-    			else if(A >= 0 && A < n && a[A] >=a[A+1] && a[A]>= a[A-1])
-    			    f=1;
-    			else
-    			    f=0;
-			    out.println(f);
-			}
-		}
-		out.close();
-	}
-}
-// } Driver Code Ends
-
 /*Complete the function below*/
 // Time Complexity: O(log(n))
 // Space Complexity; O(1)
-class Solution
-{
+class Solution {
 	// Function to find the peak element
 	// arr[]: input array
 	// n: size of array a[]
-	public int peakElement(int[] arr,int n)
-    {
-        //add code here.
-        if(n == 1){
-            return 0;
-        }
-        if(arr[0] > arr[1]){
-              return 0;
-        }
-        if(arr[n-1] > arr[n-2]){
-            return n-1;
-        }
-        int beg = 1;
-        int end = n-1;
-        while(beg <= end){
-            int mid = (beg + end) / 2;
-            if(arr[mid] >= arr[mid-1] && arr[mid] >= arr[mid+1]){
-                return mid;
-            }
-            else if(arr[mid] > arr[mid-1]){
-                beg = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
+	public int peakElement(int[] arr,int n) {
+        	//add code here.
+        	if(n == 1) {
+            		return 0;
+        	}
+        	if(arr[0] > arr[1]) {
+              		return 0;
+        	}
+        	if(arr[n-1] > arr[n-2]) {
+            		return n-1;
+        	}
+        	int beg = 1;
+        	int end = n-1;
+        	while(beg <= end){
+            		int mid = (beg + end) / 2;
+            		if(arr[mid] >= arr[mid-1] && arr[mid] >= arr[mid+1]) {
+                		return mid;
+            		}
+            		else if(arr[mid] > arr[mid-1]) {
+                		beg = mid + 1;
+            		}
+            		else {
+                		end = mid - 1;
+            		}
+        	}
+        	return -1;
+    	}
 }
 
