e151073d44447286b8de08c1657e1a031d68905e
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2024-02-24 21:29:21 +0530

    sort-an-array-of-0s-1s-and-2s.java

--- Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.java
+++ Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.java
@@ -1,11 +1,4 @@
-//{ Driver Code Starts
-//Initial template for Java
 
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 //User function template for Java
 
 class Solution {
@@ -23,31 +16,3 @@ class Solution {
         }
     }
 }
-
-//{ Driver Code Starts.
-
-class GFG {
-    
-    public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-        while(t-->0){
-            int n = Integer.parseInt(br.readLine().trim());
-            int arr[] = new int[n];
-            String inputLine[] = br.readLine().trim().split(" ");
-            for(int i=0; i<n; i++){
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            Solution ob=new Solution();
-            ob.sort012(arr, n);
-            StringBuffer str = new StringBuffer();
-            for(int i=0; i<n; i++){
-                str.append(arr[i]+" ");
-            }
-            System.out.println(str);
-        }
-    }
-}
-
-
-// } Driver Code Ends
