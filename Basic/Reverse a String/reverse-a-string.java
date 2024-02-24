45b86cd7df357c8a19330928d96f6f6fbbce0317
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2024-02-24 21:02:07 +0530

    reverse-a-string.java

--- Basic/Reverse a String/reverse-a-string.java
+++ Basic/Reverse a String/reverse-a-string.java
@@ -1,30 +1,5 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class Driver
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        
-        while(t-- >0)
-        {
-            String str = read.readLine();
-            System.out.println(new Reverse().reverseWord(str));
-        }
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-
+// Time Complexity: O(|S|)
+// Auxiliary Space: O(1)
 class Reverse {
     // Complete the function
     // str: input string
