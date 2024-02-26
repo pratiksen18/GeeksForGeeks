7620757a223d61dd3cc695719f94d1a74b574432
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2024-02-26 21:51:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().getPairsCount(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(k - arr[i])) {
+                num += map.get(k - arr[i]);
+            }
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+            else {
+                map.put(arr[i], 1);
+            }
+        }
+        return num;
+    }
+}
